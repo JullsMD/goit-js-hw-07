0d2c72503c1c5a402d419ebf4b6bf264be08550a
Julia Melnik <julls.hm.md@gmail.com>
2021-04-06 01:20:16 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
     'Зелень',
     'Приправы',
   ];
+
+const ingredientsList = document.querySelector('#ingredients');
+const ingredientsItem = ingredients.map(element =>
+  {
+    let item = document.createElement('li');
+    item.textContent = element;
+    return item;
+});
+ingredientsList.append(...ingredientsItem)
+
+
