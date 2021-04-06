e1a9f7a30302689c64c3b91d7496fc065584b3ae
Julia Melnik <julls.hm.md@gmail.com>
2021-04-07 00:09:10 +0300

    правки

--- js/task-06.js
+++ js/task-06.js
@@ -1,20 +1,15 @@
 const input = document.querySelector('#validation-input');
 
-input.addEventListener('input', () => {
-  input.value.length !== +input.getAttribute('data-length')
-    ? input.classList.add('invalid')
-    : input.classList.remove('invalid');
-});
-
-input.addEventListener('input', () => {
-  input.value.length === +input.getAttribute('data-length')
-    ? input.classList.add('valid')
-    : input.classList.remove('valid');
-});
-
-input.addEventListener('input', () => {
-  if (input.value.length === 0) {
+function validationInput() {
+  if
+  (input.value.length <input.dataset.length || input.value.length > input.dataset.length) {
+    input.classList.add('invalid');
     input.classList.remove('valid');
-    input.classList.remove('invalid');
   }
-});
+  else {
+    input.classList.remove('invalid');
+    input.classList.add('valid');
+  }
+}
+
+input.addEventListener('blur', validationInput);
